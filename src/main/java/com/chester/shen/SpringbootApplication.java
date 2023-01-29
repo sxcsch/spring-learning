8469f7622f5cb1fc6fd2260b7c062a110b804d5e
shenxiaochen <shenxiaochen@hualala.com>
2023-01-29 11:13:47 +0800

    基础学习项目

---
+++ src/main/java/com/chester/shen/SpringbootApplication.java
@@ -0,0 +1,15 @@
+package com.chester.shen;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author Chester Shen
+ * @date 2023/01/28 14:42
+ */
+@SpringBootApplication
+public class SpringbootApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(SpringbootApplication.class, args);
+    }
+}
